3ff793c24a6bf985f79766c088013f31a686e759
Mike Schutte <tmikeschutte@gmail.com>
2016-10-26 20:19:16 -0600

    refactors simpleserver. ready for master

--- lib/simple_server.rb
+++ lib/simple_server.rb
@@ -3,13 +3,17 @@ require './lib/request_lines'
 
 class SimpleServer
 
-  attr_reader :server, 
+  attr_reader :server,
+              :request, 
+              :client,
               :loop,
               :hello_hits,
               :total_hits
 
   def initialize(port = 9292)
     @server     = TCPServer.new(port)
+    @request    = nil
+    @client     = nil
     @loop       = true
     @hello_hits = 0
     @total_hits = 0
@@ -19,19 +23,19 @@ class SimpleServer
     while @loop do
       puts "\nWaiting for request..."
       run_request_response_cycle
-      puts "Response complete. \n"    
+      puts "Response complete for '#{request.path}' path. \n"    
     end
   end
 
   def run_request_response_cycle
-    client  = server.accept
-    request = request_lines(client)
-    counter_and_switch_update(request)
-    output_and_header_to_view(client, request)
+    @client  = server.accept
+    @request = request_lines
+    counter_and_switch_update
+    output_and_header_to_view
     client.close
   end
 
-  def request_lines(client)
+  def request_lines
     request = RequestLines.new
     while line = client.gets and !line.chomp.empty?
       request << line.chomp
@@ -39,42 +43,42 @@ class SimpleServer
     request
   end 
   
-  def counter_and_switch_update(request)
+  def counter_and_switch_update
     @total_hits += 1
     @hello_hits += 1     if request.path == "/hello"
     @loop        = false if request.path == "/shutdown"
   end
 
-  def output_and_header_to_view(client, request)
-    client.puts headers(request)
-    client.puts output(request)
+  def output_and_header_to_view
+    client.puts headers
+    client.puts output
   end
 
-  def headers(request)
+  def headers
     ["http/1.1 200 ok",
     "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
     "server: ruby",
     "content-type: text/html; charset=iso-8859-1",
-    "content-length: #{output(request).length}\r\n\r\n"].join("\r\n")
+    "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   end
 
-  def output(request)
-    "<html><head></head><body>#{response(request)}</body></html>"
+  def output
+    "<html><head></head><body>#{response}</body></html>"
   end
 
-  def response(request)
-    body(request).join("\n")
+  def response
+    body.join("\n")
   end
 
-  def body(request)
-    [content(request),  diagnostics(request)]
+  def body
+    [content,  diagnostics]
   end
 
-  def content(request)
-    request.handle(@hello_hits, @total_hits)
+  def content
+    request.handle(hello_hits, total_hits)
   end    
 
-  def diagnostics(request)
+  def diagnostics
     request.diagnostics
   end
 
